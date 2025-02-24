978e4b5424385fe03e5ac26412356b31701d6f3a
osic <fortyone@live.at>
2025-02-24 14:05:36 +0100

    feat(nvim-treesitter): configure language parsers and enhance syntax features
    - Added full configuration for nvim-treesitter
    - Ensured installation of language parsers: bash, html, http, javascript, json, lua, markdown, python, regex, text, tsx, typescript, vim, yaml
    - Enabled Treesitter-based syntax highlighting, indentation, and folding
    - Excluded 'kulala_verbose_result' to avoid conflicts with custom filetype

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,35 +1,35 @@
 return {
-  -- "nvim-treesitter/nvim-treesitter",
-  -- opts = {
-  --   ensure_installed = {
-  --     "bash",
-  --     "html",
-  --     "http", -- <- Make sure http is included
-  --     "javascript",
-  --     "json",
-  --     "lua",
-  --     "markdown",
-  --     "markdown_inline",
-  --     "python",
-  --     "query",
-  --     "regex",
-  --     "text",
-  --     "tsx",
-  --     "typescript",
-  --     "vim",
-  --     "yaml",
-  --   },
-  --   highlight = {
-  --     enable = true,
-  --     disable = { "kulala_verbose_result" },
-  --   },
-  --   indent = {
-  --     enable = true,
-  --     disable = { "kulala_verbose_result" },
-  --   },
-  --   fold = {
-  --     enable = true,
-  --     disable = { "kulala_verbose_result" },
-  --   },
-  -- },
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "html",
+      "http", -- <- Make sure http is included
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "text",
+      "tsx",
+      "typescript",
+      "vim",
+      "yaml",
+    },
+    highlight = {
+      enable = true,
+      disable = { "kulala_verbose_result" },
+    },
+    indent = {
+      enable = true,
+      disable = { "kulala_verbose_result" },
+    },
+    fold = {
+      enable = true,
+      disable = { "kulala_verbose_result" },
+    },
+  },
 }
