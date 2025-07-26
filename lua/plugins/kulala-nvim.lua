eff95e1fe6a158c2e0b89ef1d466c844475cf609
osic <fortyone@live.at>
2025-07-26 19:16:30 +0200

    update config
    - add note to kulala to install jq
    - comment out brave(windows(for windows))

--- lua/plugins/kulala-nvim.lua
+++ lua/plugins/kulala-nvim.lua
@@ -1,30 +1,23 @@
+-- NOTE: make sure jq is installed
+-- sudo apt install jq
 return {
   "mistweaverco/kulala.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   opts = {},
   config = function()
     require("kulala").setup({
-      -- Default settings; adjust as needed
       result = {
-        format = "json", -- Automatically formats JSON responses
-        show_headers = true, -- Show headers in the response pane
+        format = "json",
+        show_headers = true,
       },
       opts = {
         window = {
-          position = "right", -- or "bottom"
-          width = 50, -- Adjust to your preference
+          position = "right",
+          width = 30,
           height = 20,
         },
       },
     })
-
-    -- Map <leader>rr to run the request under the cursor
-    -- vim.api.nvim_set_keymap(
-    --   "n",
-    --   "<leader>rr",
-    --   "<cmd>lua require('kulala').run()<CR>",
-    --   { noremap = true, silent = true, desc = "Run Request" }
-    -- )
     vim.filetype.add({
       extension = {
         ["http"] = "http",
